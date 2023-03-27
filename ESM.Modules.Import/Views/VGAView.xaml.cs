1a9444b459e0f465b8c6293bb14df2c08ee1cafa
Nhựt Phạm Quang <102210351@sv1.dut.udn.vn>
2023-03-27 23:52:15 +0700

    Tôi đéo làm gì

---
+++ ESM.Modules.Import/Views/VGAView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ESM.Modules.Import.Views
+{
+    /// <summary>
+    /// Interaction logic for VGAView
+    /// </summary>
+    public partial class VGAView : UserControl
+    {
+        public VGAView()
+        {
+            InitializeComponent();
+        }
+    }
+}
