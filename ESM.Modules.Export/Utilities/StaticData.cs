27d768f332fb7a6999b42eb90525d188b3f8f990
Trương Công Hoàng Long <truongconghoanglong@gmail.com>
2023-04-01 14:03:47 +0700

    getExportUtilites

---
+++ ESM.Modules.Export/Utilities/StaticData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESM.Modules.Export.Utilities
+{
+    internal enum ReportDuration
+    { WEEK, MONTH, QUARTER, YEAR }
+    internal class StaticData
+    {
+    }
+}
