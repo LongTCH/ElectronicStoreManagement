ab14d4a4e94dc0eb8daa333b708de8acf8e929a7
Trương Công Hoàng Long <truongconghoanglong@gmail.com>
2023-04-02 23:27:19 +0700

    Change Report to Tabregion

---
+++ ESM.Modules.Export/ViewModels/TabChartViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ESM.Modules.Export.ViewModels
+{
+    public class TabChartViewModel : BindableBase
+    {
+        public TabChartViewModel()
+        {
+
+        }
+    }
+}
