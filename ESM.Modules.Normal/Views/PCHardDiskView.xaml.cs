c4efb257454640dd05f968c9af0b30c52d31a169
Trương Công Hoàng Long <truongconghoanglong@gmail.com>
2023-04-15 14:00:40 +0700

    Fix Scroll Problem

--- ESM.Modules.Normal/Views/PCHardDiskView.xaml.cs
+++ ESM.Modules.Normal/Views/PCHardDiskView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace ESM.Modules.Normal.Views
 {
@@ -10,6 +11,11 @@ namespace ESM.Modules.Normal.Views
         public PCHardDiskView()
         {
             InitializeComponent();
+        }
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ScrollViewer viewer = sender as ScrollViewer;
+            viewer.ScrollToVerticalOffset(viewer.VerticalOffset - e.Delta);
         }
     }
 }
