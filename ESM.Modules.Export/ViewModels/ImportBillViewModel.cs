f0c0b049898b60d06a60d23e54acb8f068c54f5a
Trương Công Hoàng Long <truongconghoanglong@gmail.com>
2023-04-19 13:14:40 +0700

    catch not enter info ImportBill

--- ESM.Modules.Export/ViewModels/ImportBillViewModel.cs
+++ ESM.Modules.Export/ViewModels/ImportBillViewModel.cs
@@ -5,16 +5,13 @@ using ESM.Modules.DataAccess;
 using ESM.Modules.DataAccess.Infrastructure;
 using ESM.Modules.DataAccess.Models;
 using ESM.Modules.Export.Utilities;
-using ESM.Modules.Export.Views;
 using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows;
 
 namespace ESM.Modules.Export.ViewModels
 {
@@ -129,6 +126,21 @@ namespace ESM.Modules.Export.ViewModels
         }
         private async Task ExecutePay()
         {
+            if (string.IsNullOrWhiteSpace(ProviderName))
+            {
+                _modalService.ShowModal(ModalType.Error, "Nhập tên nhà cung cấp", "Thông báo");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(ImportBillId))
+            {
+                _modalService.ShowModal(ModalType.Error, "Nhập mã hóa đơn nhập hàng", "Thông báo");
+                return;
+            }
+            if (!ImportDate.HasValue)
+            {
+                _modalService.ShowModal(ModalType.Error, "Nhập ngày nhập hàng", "Thông báo");
+                return;
+            }
             ICollection<ImportProduct> billProducts = new List<ImportProduct>();
             foreach (var item in ProductBillList)
             {
